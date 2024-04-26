6ea2962697ff13fddab434d66f7379e353369089
Daniel Olsrød <daniel.olsrod@gmail.com>
2024-04-26 08:43:58 +0200

    added file sturctiure

--- JS/config.js
+++ JS/config.js
@@ -4,6 +4,9 @@
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js"
+
+
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyD35laqRB6A1et7qFeyyCrf8ujKsY8nIIk",
---
+++ JS/script.js
@@ -0,0 +1,2 @@
+import { db } from "./config.js"
+import { getDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
