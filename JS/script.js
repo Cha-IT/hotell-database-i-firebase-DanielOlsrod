48e4876ff7d1bafe6918619db78b49e7f5dd8888
Daniel Olsrød <daniel.olsrod@gmail.com>
2024-04-30 12:12:13 +0200

    made some changes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,28 @@
 import { db } from "./config.js"
-import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { 
+    getFirestore,
+    collection,
+    getDocs,
+    
+    
+}from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
+
+
+
+
+
+
+/*const colRef = collection(db, "Room")
+
+getDocs(colRef)
+    .then((snapshot) => {
+        let rooms = []
+        snapshot.docs.forEach((doc) => {
+            rooms.push({ ...doc.data(), id: doc.id})
+        })
+        console.log(rooms)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })*/
